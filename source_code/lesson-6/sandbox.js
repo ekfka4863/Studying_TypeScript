0596a03e1984ce5aaf5cd17d4304b391824342d3
ekfka4863 <ekfka4863@gmail.com>
2022-05-14 20:56:33 +0900

    [ADD] how to achieve better workflow using tsconfig.json

--- source_code/lesson-6/sandbox.js
+++ source_code/lesson-6/sandbox.js
@@ -1,24 +1,17 @@
-let age: any = 25;
-
+var age = 25;
 age = true;
 console.log(age);
 age = 'hello';
 console.log(age);
 age = { name: 'luigi' };
 console.log(age);
-
-let mixed: any[] = [];  // cf. array mixed can contain any type of values
-
+var mixed = []; // cf. array mixed can contain any type of values
 mixed.push(5);
 mixed.push('mario');
 mixed.push(false);
 console.log(mixed);
-
-let ninja: { name: any, age: any }; // cf. inside of an object called ninja any type of values can be bound to 'name' - and 'age' property
-
+var ninja; // cf. inside of an object called ninja any type of values can be bound to 'name' - and 'age' property
 ninja = { name: 'yoshi', age: 25 };
 console.log(ninja);
-
 ninja = { name: 25, age: 'yoshi' };
 console.log(ninja);
-
